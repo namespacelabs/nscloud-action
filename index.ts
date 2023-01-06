ee2a3dd0300ef425589ebcd5e5d56c01a2dd38b9
Niklas Gehlen <niklas@namespacelabs.com>
2023-01-06 14:46:04 +0100

    First demo action attempt

---
+++ index.ts
@@ -0,0 +1,14 @@
+import * as core from "@actions/core";
+
+async function run() {
+	const isDefined = (i) => !!i;
+
+	const { GITHUB_REPOSITORY, GITHUB_ACTOR, GITHUB_SHA } = process.env;
+	if (![GITHUB_REPOSITORY, GITHUB_ACTOR, GITHUB_SHA].every(isDefined)) {
+		core.setFailed("Missing required environment value. Are you running in GitHub Actions?");
+	}
+
+	core.notice(`repo: ${GITHUB_REPOSITORY}, author: ${GITHUB_ACTOR}, commit: ${GITHUB_SHA}`);
+}
+
+run();
