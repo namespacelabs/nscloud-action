721c0d2b267db32a52a03097c85e8e3ca63a2496
Niklas Gehlen <niklas@namespacelabs.com>
2023-03-07 15:28:46 +0100

    use nscloud-setup action Detect if it is missing in the workflow

--- common.ts
+++ common.ts
@@ -1,5 +1,3 @@
-import * as core from "@actions/core";
-import * as tc from "@actions/tool-cache";
 import * as path from "path";
 import * as fs from "fs";
 import * as exec from "@actions/exec";
@@ -14,52 +12,6 @@ export function tmpFile(file: string): string {
 	}
 
 	return path.join(tmpDir, file);
-}
-
-export async function installNsc() {
-	// Download the specific version of the tool, e.g. as a tarball
-	const pathToTarball = await tc.downloadTool(getDownloadURL(), null, null, {
-		CI: process.env.CI,
-		"User-Agent": "nscloud-action",
-	});
-
-	// Extract the tarball onto the runner
-	const pathToCLI = await tc.extractTar(pathToTarball);
-
-	// Expose the tool by adding it to the PATH
-	core.addPath(pathToCLI);
-
-	core.exportVariable("NS_DO_NOT_UPDATE", "true");
-}
-
-function getDownloadURL(): string {
-	const { RUNNER_ARCH, RUNNER_OS } = process.env;
-
-	let arch = "";
-	switch (RUNNER_ARCH) {
-		case "X64":
-			arch = "amd64";
-			break;
-		case "ARM64":
-			arch = "arm64";
-			break;
-		default:
-			throw new Error(`Unsupported architecture: ${RUNNER_ARCH}`);
-	}
-
-	let os = "";
-	switch (RUNNER_OS) {
-		case "macOS":
-			os = "darwin";
-			break;
-		case "Linux":
-			os = "linux";
-			break;
-		default:
-			throw new Error(`Unsupported operating system: ${RUNNER_OS}`);
-	}
-
-	return `https://get.namespace.so/packages/nsc/latest?arch=${arch}&os=${os}`;
 }
 
 export async function ensureFreshTenantToken() {
--- main.ts
+++ main.ts
@@ -1,14 +1,24 @@
 import * as core from "@actions/core";
 import * as fs from "fs";
 import * as exec from "@actions/exec";
-import { ClusterIdKey, ensureFreshTenantToken, installNsc, tmpFile } from "./common";
+import { ClusterIdKey, ensureFreshTenantToken, tmpFile } from "./common";
 
 async function run(): Promise<void> {
+	var commandExists = require("command-exists");
+
+	await commandExists("nsc")
+		.then(createCluster)
+		.catch(function () {
+			core.setFailed(`Namespace Cloud CLI not found.
+
+Please add a step this step to your workflow's job definition:
+
+- uses: namespacelabs/nscloud-setup@v0.0.1`);
+		});
+}
+
+async function createCluster(): Promise<void> {
 	try {
-		await installNsc();
-
-		core.setCommandEcho(true);
-
 		// Start downloading kubectl while we prepare the cluster.
 		const kubectlDir = downloadKubectl();
 
